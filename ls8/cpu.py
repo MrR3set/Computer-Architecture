528aeb9977750a52dcecdbae362bc88ac135c115
MrR3set <bairon.pazrobles@gmail.com>
2020-08-21 08:58:53 -0700

    sprint

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -5,9 +5,22 @@ import sys
 LDI = 0b10000010
 PRN = 0b01000111
 HLT = 0b00000001
-MUL = 0b10100010
 PUSH = 0b01000101
 POP = 0b01000110
+CALL = 0b01010000
+RET  = 0b00010001
+JMP  = 0b01010100
+JNE  = 0b01010110
+JEQ  = 0b01010101
+MUL = 0b10100010
+CMP  = 0b10100111
+AND  = 0b10100000
+OR   = 0b10101010
+XOR  = 0b10101011
+NOT  = 0b01101001
+SHL  = 0b10101100
+SHR  = 0b10101101
+MOD  = 0b10100100
 
 class CPU:
     """Main CPU class."""
@@ -18,7 +31,12 @@ class CPU:
         self.ram = [0]*256
         self.reg = [0]*8 
         self.reg[7] = 0xF4 # SP
-        self.sp = 7
+        self.sp = self.reg[7]
+
+        self.L = 0
+        self.G = 0
+        self.E = 0
+
 
     def load(self, file):
         """Load a program into memory."""
@@ -44,6 +62,13 @@ class CPU:
         #elif op == "SUB": etc
         elif op == "MUL":
             self.reg[reg_a] *= self.reg[reg_b]
+        elif op == "CMP":
+            if self.reg[reg_a]>self.reg[reg_b]:
+                self.G=1
+            elif self.reg[reg_a]<self.reg[reg_b]:
+                self.L=1
+            else:
+                self.E = 1
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -92,17 +117,47 @@ class CPU:
                 self.alu("MUL",reg_a,reg_a+1)
                 self.pc += 3
             elif IR == PUSH:
+                self.sp -= 1
                 reg_a = self.ram_read(self.pc+1)
                 value = self.reg[reg_a]
                 self.reg[reg_a] -= 1
-                self.ram[self.reg[self.sp]]=value
+                self.ram[self.sp]=value
                 self.pc += 2
-
             elif IR == POP:
                 reg_a = self.ram_read(self.pc+1)
-                self.reg[reg_a] = self.ram[self.reg[self.sp]]
-                self.reg[self.sp] += 1
+                self.reg[reg_a] = self.ram[self.sp]
+                self.sp += 1
                 self.pc+=2
+            elif IR == CALL:
+                value = self.pc + 2
+                self.pc -= 1
+                self.ram[self.sp] = value
+                value = self.ram[self.pc+1]
+                self.pc = self.reg[value]
+            elif IR == RET:
+                value = self.ram[self.sp]
+                self.sp += 1
+                self.pc = value
+            elif IR == CMP:
+                reg_a = self.ram_read(self.pc+1)
+                self.alu("CMP",reg_a,reg_a+1)
+                self.pc+=3
+            elif IR == JMP:
+                value = self.ram[self.pc+1] # Jump to the address stored in the given register.
+                self.pc = self.reg[value] # Set the `PC` to the address stored in the given register.
+            elif IR == JNE:
+                # If `E` flag is clear (false, 0), jump to the address stored in the given register.
+                if self.E == 0:
+                    value = self.ram[self.pc+1]
+                    self.pc = self.reg[value]
+                else:
+                    self.pc += 2
+            elif IR == JEQ:
+                if self.E == 1:
+                    value = self.ram[self.pc+1]
+                    self.pc = self.reg[value]
+                else:
+                    self.pc += 2
 
 
 
